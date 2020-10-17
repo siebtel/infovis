29be31dfeb0bcbe39edcfdfed14ed8129ba9eb76
siebtel <siebtel.ss@gmail.com>
2020-10-17 12:05:10 -0300

    added text inside the box

--- index/index.js
+++ index/index.js
@@ -1,10 +1,11 @@
 let text = ['to be, or not to be, that is the question']
 let tokenized_text = ['to', 'be', 'or', 'not', 'to','be', 'that','is' ,'the' ,'question']
 
-var svg = d3.select("svg");
-var text = svg.selectAll('text')
+var svg = d3.select("div");
+svg.selectAll('p')
     .data(tokenized_text)
-    .enter().append('text')
-    .attr("x", function(d,i){return 100 * (i % 3)})
+    .enter()
+    .append('p')
+    .attr("x", function(d,i){return 100 * ((i*2) % 3)})
     .attr("y", function(d,i){return 20 * ( Math.floor(i/3) ) })
     .text(function(d){return d});

8ea39f4ac3ebb63af40e22f8723a9aa258b3d88e
siebtel <siebtel.ss@gmail.com>
2020-10-23 16:53:11 -0300

    added special characters removal

--- index/index.js
+++ index/index.js
@@ -1,3 +1,124 @@
+//-----------------------------------------------functions------------------------------------------------
+function setNodesPreProcessing() {//setNodesPartOne
+	// Here we're setting the nodes
+	g.setNode('sentence vector', {
+	  label: original_sentence,
+	  style: 'fill: aqua'
+	});
+	g.setNode('pre_processing', {//
+	  label: 'Pre Processing',
+	  clusterLabelPos: 'top',
+	  style: 'fill: white'
+	});
+	g.setNode('array_form', {
+	  label: 'array form',
+	  clusterLabelPos: 'bottom',
+	  style: 'fill: #ffd47f'
+	});
+	// Set the parents to define which nodes belong to which cluster
+	g.setParent('array_form', 'pre_processing');//
+	g.setParent('sentence vector', 'array_form');
+	// Set up edges, no special attributes.
+	g.setEdge('original sentence', 'sentence vector');//
+	// Round the corners of the nodes
+	g.nodes().forEach(function (v) {
+	  var node = g.node(v);
+	  node.rx = node.ry = 5;
+	});
+  }
+function setNodesTokenization() {//setNodesPartTwo
+	// Here we're setting the nodes
+	tokens.forEach(setTokenNode);
+	g.setNode('tokenization', {
+	  label: 'Tokens',
+	  style: 'fill: #5f9488'
+	});
+	// Set the parents to define which nodes belong to which cluster
+	g.setParent('tokenization', 'pre_processing');
+	var i;
+	for (i = 0; i < tokens.length; i++) {
+	  g.setParent('token'.concat(i.toString()), 'tokenization');
+	}
+	// Set up edges, no special attributes.
+	for (i = 0; i < tokens.length; i++) {
+	  g.setEdge('sentence vector', 'token'.concat(i.toString()));
+	}
+	// Round the corners of the nodes
+	g.nodes().forEach(function (v) {
+	  var node = g.node(v);
+	  node.rx = node.ry = 5;
+	});
+  }
+function setLowcaseTokens() {
+	// Here we're setting the nodes
+	lowcase_tokens.forEach(setTokenNode);
+	g.setNode('low_case', {
+		label: 'Lower case',
+		style: 'fill: #5f9488'
+	  });
+	// Set the parents to define which nodes belong to which cluster
+	g.setParent('low_case', 'pre_processing');
+	for (i = tokens.length; i < tokens.length + lowcase_tokens.length; i++) {
+		g.setParent('token'.concat(i.toString()), 'low_case');
+	}
+	// Set up edges, no special attributes.
+	for (i = 0; i < tokens.length; i++) {
+		var lowcase_index = i + tokens.length;
+		g.setEdge('token'.concat(i.toString()), 'token'.concat(lowcase_index.toString()));
+	}
+	// Round the corners of the nodes
+	g.nodes().forEach(function(v) {
+		var node = g.node(v);
+		node.rx = node.ry = 5;
+	});
+}
+
+function setNoSpecialCharTokens() {
+	// Here we're setting the nodes
+	no_special_char_tokens.forEach(setTokenNode);
+	g.setNode('no_special_character_token', {
+		label: 'Removing Special Characters',
+		style: 'fill: #5f9488'
+	  });
+	// Set the parents to define which nodes belong to which cluster
+	g.setParent('no_special_character_token', 'pre_processing');
+	var sum_of_nodes_of_tokens = tokens.length + lowcase_tokens.length
+	for (i = sum_of_nodes_of_tokens; i < sum_of_nodes_of_tokens + no_special_char_tokens.length; i++) {
+		g.setParent('token'.concat(i.toString()), 'no_special_character_token');
+	}
+	// Set up edges, no special attributes.
+	var format = /^[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]*$/;
+	for (i = 0; i < tokens.length; i++) {
+		if (tokens[i].match(format)){
+			continue;
+		} 
+		var no_special_character_token_index = i + sum_of_nodes_of_tokens;
+		var lowcase_index = i + tokens.length;
+		g.setEdge('token'.concat(lowcase_index.toString()), 'token'.concat(no_special_character_token_index.toString()));
+	}
+	// Round the corners of the nodes
+	g.nodes().forEach(function(v) {
+		var node = g.node(v);
+		node.rx = node.ry = 5;
+	});
+}
+function setTokenNode(item, index, arr) {
+	if (arr == lowcase_tokens) {
+	  var index = index + tokens.length;
+	} else if (arr == no_special_char_tokens) {
+	  var index = index + lowcase_tokens.length + tokens.length;
+	}
+  
+	g.setNode('token'.concat(index.toString()), {
+	  label: item
+	});
+}
+function centerGraph() {
+	var xCenterOffset = (svg.attr("width") - g.graph().width) / 2;
+	svgGroup.attr("transform", "translate(" + xCenterOffset + ", 20)");
+	svg.attr("height", g.graph().height + 40);
+}
+// --------------------------------------------------------main------------------------------------------------------
 // Create the input graph
 var g = new dagreD3.graphlib.Graph({
   compound: true
@@ -9,7 +130,7 @@ var g = new dagreD3.graphlib.Graph({
 var original_sentence = 'Computer Science is no more about computers than Astronomy is about telescopes.';
 var tokens = ['Computer', 'Science', 'is', 'no', 'more', 'about', 'computers', 'than', 'Astronomy', 'is', 'about', 'telescopes.'];
 var lowcase_tokens = ['computer', 'science', 'is', 'no', 'more', 'about', 'computers', 'than', 'astronomy', 'is', 'about', 'telescopes.'];
-var just_words_tokens = ['computer', 'science', 'is', 'no', 'more', 'about', 'computers', 'than', 'astronomy', 'is', 'about', 'telescopes'];
+var no_special_char_tokens = ['computer', 'science', 'is', 'no', 'more', 'about', 'computers', 'than', 'astronomy', 'is', 'about', 'telescopes'];
 var tokens_with_removed_stopwords = ['computer', 'science', 'computers', 'astronomy', 'telescopes'];
 var stopwords = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', 'your', 'yours', 'yourself', 'yourselves',
   'he', 'him', 'his', 'himself', 'she', 'her', 'hers', 'herself', 'it', 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves',
@@ -61,7 +182,8 @@ svg1.append('rect')//Segundo botão p/ acionar
     svg.selectAll("*").remove();
 
     setNodesTokenization();
-
+	setLowcaseTokens();
+	setNoSpecialCharTokens();
     render(d3.select("svg g"), g);
 
     centerGraph();
@@ -69,95 +191,11 @@ svg1.append('rect')//Segundo botão p/ acionar
     d3.select(this).remove();
   });
 
-function setNodesPreProcessing() {//setNodesPartOne
-  // Here we're setting the nodes
-  g.setNode('sentence vector', {
-    label: original_sentence,
-    style: 'fill: aqua'
-  });
-
-  g.setNode('pre_processing', {//
-    label: 'Pre Processing',
-    clusterLabelPos: 'top',
-    style: 'fill: white'
-  });
-  g.setNode('array_form', {
-    label: 'array form',
-    clusterLabelPos: 'bottom',
-    style: 'fill: #ffd47f'
-  });
-  g.setParent('array_form', 'pre_processing');//
-  g.setParent('sentence vector', 'array_form');
-  g.setEdge('original sentence', 'sentence vector');//
-
-  g.nodes().forEach(function (v) {
-    var node = g.node(v);
-    // Round the corners of the nodes
-    node.rx = node.ry = 5;
-  });
-}
-
-function setNodesTokenization() {//setNodesPartTwo
-  tokens.forEach(setTokenNode);
-  lowcase_tokens.forEach(setTokenNode);
-
-  g.setNode('tokenization', {
-    label: 'Tokens',
-    style: 'fill: #5f9488'
-  });
-  g.setNode('low_case', {
-    label: 'Lower case',
-    style: 'fill: #5f9488'
-  });
-  g.setParent('tokenization', 'pre_processing');
-  g.setParent('low_case', 'pre_processing');
-  setNodesPreProcessing();//setNodesPartOne
-  var i;
-  for (i = 0; i < tokens.length; i++) {
-    g.setParent('token'.concat(i.toString()), 'tokenization');
-  }
-  for (i = tokens.length; i < tokens.length + lowcase_tokens.length; i++) {
-    g.setParent('token'.concat(i.toString()), 'low_case');
-  }
-  // Set up edges, no special attributes.
-  for (i = 0; i < tokens.length; i++) {
-    g.setEdge('sentence vector', 'token'.concat(i.toString()));
-  }
-  for (i = 0; i < tokens.length; i++) {
-    var lowcase_index = i + tokens.length;
-    g.setEdge('token'.concat(i.toString()), 'token'.concat(lowcase_index.toString()));
-  }
-  g.nodes().forEach(function (v) {
-    var node = g.node(v);
-    // Round the corners of the nodes
-    node.rx = node.ry = 5;
-  });
-}
-
-function setTokenNode(item, index, arr) {
-  if (arr == lowcase_tokens) {
-    var index = index + tokens.length;
-  } else if (arr == just_words_tokens) {
-    var index = index + lowcase_tokens.length + tokens.length;
-  }
-
-  g.setNode('token'.concat(index.toString()), {
-    label: item
-  });
-}
-
 g.nodes().forEach(function (v) {
   var node = g.node(v);
   // Round the corners of the nodes
   node.rx = node.ry = 5;
 });
-
-// Center the graph
-function centerGraph() {
-  var xCenterOffset = (svg.attr("width") - g.graph().width) / 2;
-  svgGroup.attr("transform", "translate(" + xCenterOffset + ", 20)");
-  svg.attr("height", g.graph().height + 40);
-}
 
 // Create the renderer
 var render = new dagreD3.render();

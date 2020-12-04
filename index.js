8ce530dfccc02a57c3efc13dc46737e2561c10f1
siebtel <siebtel.ss@gmail.com>
2020-12-04 00:41:46 -0300

    Layout changes, code refactoring with sentence generalization
    Co-authored-by: @avmss3101 @marceloschots

--- index.js
+++ index.js
@@ -5,23 +5,33 @@ function display(stepCallback, highlited) {
 }
 
 function set_explanation(index) {
-  explanation.transition().style("opacity", 0).duration(800);	
+  explanation.transition().style("opacity", 0).duration(800);
   explanation.transition().delay(800).text(texts[index]);
   explanation.transition().delay(800).style("opacity", 1).duration(1000);
 }
 
 function tokenization() {
   set_explanation(1);
-  d3.select("#sentence").transition().style("opacity", 0).duration(1000);
-  d3.select("#tokenization").transition().delay(999).attr("opacity", 1).duration(1000);
+  // resetting eventual accomulations of bracets sekected in previous interactions
+
+  d3.select("#sentence").text().replaceAll("[", "").replaceAll("]", "").replaceAll(",", ""); // it is not a good idea to replace commas
+
+  //d3.select("#sentence").text().replaceAll(/(\],)+?, (\[)+?/g, "");
+  // replace logi
+  var tokenized = "[" + d3.select("#sentence").text().replaceAll(" ", "], [") + "]";
+  d3.select("#sentence").transition().style("opacity", 0).duration(500);
+  d3.select("#sentence").transition().delay(500).text(tokenized);
+  d3.select("#sentence").transition().delay(500).style("opacity", 1).duration(500);
+  //d3.select("#sentence").transition().style("opacity", 0).duration(1000);
+  //d3.select("#tokenization").transition().delay(999).attr("opacity", 1).duration(1000);
 }
 
 function lowercase() {
-  tokenization();
+  //tokenization();
   set_explanation(2);
-	d3.selectAll(".low").transition().style("fill", "red").duration(1000);
+  d3.selectAll(".low").transition().style("fill", "red").duration(1000);
   d3.select("#tokenization").transition().delay(2000).attr("class", "lowerCase");
-  d3.selectAll(".low").transition().delay(2000).style("fill", "black").duration(1000);
+  //d3.selectAll(".low").transition().delay(2000).style("fill", "black").duration(1000);
 }
 
 function removingSpecial() {
@@ -49,7 +59,7 @@ function stemming() {
 var svg = d3.select("div");
 var sentenca = d3.select("#sentence");
 var tokens = d3.select("#tokenization");
-var explanation = d3.selectAll("section text");
+var explanation = d3.selectAll("div#section span");
 var texts = ["An example of pre-processing in Natural Language Processing. This visualization shows examples of techniques used to transform raw text into just a text that we need.", "Take the sentence and divide into various tokens, i.e. segment of the text.",
   "Turns all uppercased character into lowercase", "Removes all special characters.",
   "A stopword is an ordinarily used word on a text (e.g. “is”, “a”, “an”, “on”). We usually remove them to remain only significant words.",
